8848fd502920661b6078d1a46d49f795404c4e61
Denka88 <animedenisik.com@mail.ru>
2025-01-28 21:48:06 +0300

    init

---
+++ src/main/java/com/example/mvclearn/MvcLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvclearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcLearnApplication.class, args);
+    }
+
+}
